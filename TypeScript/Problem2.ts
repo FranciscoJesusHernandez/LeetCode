138a6fb5e77728c150a976f6878623bb3949a590
Francisco Jesus Hernandez <jesus.hernandex@192.168.1.235>
2025-08-06 12:32:13 -0600

    Refactored for better understability

--- TypeScript/Problem2.ts
+++ TypeScript/Problem2.ts
@@ -1,21 +1,22 @@
 function minPatches(nums: number[], n: number): number {
-    let c = 0;
-    let last = 0;
+    let patches = 0;
+    let lastReached = 0;
+    
     for (const a of nums) {
         if (a > n) break;
 
-        while (last + 1 < a) {
-            last = 2 * last + 1;
-            c++;
+        while (lastReached + 1 < a) {
+            lastReached = 2 * lastReached + 1;
+            patches++;
         }
 
-        last += a;
+        lastReached += a;
     }
 
-    while (last < n) {
-        last += last + 1;
-        c++;
+    while (lastReached < n) {
+        lastReached += lastReached + 1;
+        patches++;
     }
 
-    return c;
+    return patches;
 };
